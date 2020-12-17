c1a3d29b831f73463b941dc8b4087243f207c54a
waterbook59 <takeshi.m64@gmail.com>
2020-12-17 13:27:05 +0900

    設定ページの大枠作成

---
+++ lib/views/settings/setting_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('開発タスク一覧'),
+          Text('通知関連'),
+          Text('ご要望・お問い合わせ'),
+          Text('レビューを書く'),
+          Text('カテゴリ設定'),
+          Text('アカウント連携(google,apple,電話番号)'),
+          //アカウント削除するとログイン画面
+          Text('利用規約'),
+          Text('プライバシーポリシー'),
+          Text('バージョン番号'),
+          Text('追加機能について(リストアも)'),
+        ],
+      )
+
+    );
+  }
+}
