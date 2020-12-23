f282f83cf92b8c9c75b9738245b053339910d934
waterbook59 <takeshi.m64@gmail.com>
2020-12-23 13:32:13 +0900

    DataListPage:Firebaseからの読取結果を表示 リストの画像をcachedImageに変更するとこでオフラインでも表示可能

--- lib/views/data_list_page/components/food_stuff_item.dart
+++ lib/views/data_list_page/components/food_stuff_item.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:datebasejointest/data_models/menu/food_stuff.dart';
 import 'package:datebasejointest/data_models/menu/food_stuff_firebase.dart';
 import 'package:flutter/material.dart';
@@ -29,13 +30,21 @@ class FoodStuffItem extends StatelessWidget {
         child: ListTile(
           leading:
 //          Image.file(File(foodStuff.localImagePath),
-            Image.network(
-                foodStuff.imageUrl,
-            ///登録リストの画像の大きさを調整
-            height: 100,
-            width: 100,
-            fit: BoxFit.cover,
-          ),
+///リスト画像をcachedImageに変えてみる
+                  CachedNetworkImage(
+        imageUrl: foodStuff.imageUrl,
+        fit: BoxFit.cover,
+        placeholder: (context,url)=>const CircularProgressIndicator(),
+        ///errorにdynamic型明示
+        errorWidget: (context,url,dynamic error)=>const Icon(Icons.broken_image),
+      ),
+//            Image.network(
+//                foodStuff.imageUrl,
+//            ///登録リストの画像の大きさを調整
+//            height: 100,
+//            width: 100,
+//            fit: BoxFit.cover,
+//          ),
           title: Text("${foodStuff.name}", style: TextStyle(color: Colors.black87,fontSize: 23.0),),
           //todo 期限を○年○月○日表記へ変更
           subtitle: Text("在庫：${foodStuff.amount}・期限：${foodStuff.validDate}",
